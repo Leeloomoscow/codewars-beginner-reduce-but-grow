836ac275cc585ebfa9c0e0d5390715aed6b66230
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-09-14 20:21:42 +0300

    Codewars beginner reduce but grow

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,8 +1,24 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyTest {
     @Test
-    void name() {
+    public void testSomething() {
+        assertEquals(6, Kata.grow(new int[]{1, 2, 3}));
+        assertEquals(16, Kata.grow(new int[]{4, 1, 1, 1, 4}));
+        assertEquals(64, Kata.grow(new int[]{2, 2, 2, 2, 2, 2}));
+    }
 
+    private static class Kata {
+        public static int grow(int[] x) {
+            int result = 1;
+            for (int x1 : x) {
+               result *= x1;
+            }
+            return result;
+        }
     }
 }
+
+
